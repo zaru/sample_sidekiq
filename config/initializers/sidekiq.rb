4d8a691e4803a9b2c7c5d929c1468f2cc031b233
zaru <zarutofu@gmail.com>
2015-05-14 23:43:22 +0000

    change sidekiq redis server hostname

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,22 +1,22 @@
 Sidekiq.configure_server do |config|
   case Rails.env
     when 'production' then
-      config.redis = { url: 'redis://prd.redis-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: 'redis://192.168.33.40:6379', namespace: 'sidekiq' }
     when 'staging' then
-      config.redis = { url: 'redis://stg-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: 'redis://192.168.33.40:6379', namespace: 'sidekiq' }
     else
-      config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
+      config.redis = { url: 'redis://192.168.33.40:6379', namespace: 'sidekiq' }
   end
 end
 
 Sidekiq.configure_client do |config|
   case Rails.env
     when 'production' then
-      config.redis = { url: 'redis://prd.redis-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: 'redis://192.168.33.40:6379', namespace: 'sidekiq' }
     when 'staging' then
-      config.redis = { url: 'redis://stg-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: 'redis://192.168.33.40:6379', namespace: 'sidekiq' }
     else
-      config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
+      config.redis = { url: 'redis://192.168.33.40:6379', namespace: 'sidekiq' }
   end
 end
 
