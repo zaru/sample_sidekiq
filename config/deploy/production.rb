fb4e8d1cdeb7e8850dd2b76585fc2169e0736187
zaru <zarutofu@gmail.com>
2015-05-14 23:43:04 +0000

    add batch server config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,16 @@
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
-#set :sidekiq_role, :web
+set :pty, false
+set :sidekiq_role, :batch
 
 role :app, %w{vagrant@192.168.33.40}
 role :web, %w{vagrant@192.168.33.40}
 role :db,  %w{vagrant@192.168.33.40}
+role :batch,  %w{vagrant@192.168.33.41}
 
 
 server '192.168.33.40', user: 'vagrant', roles: %w{web app}
+server '192.168.33.41', user: 'vagrant', roles: %w{batch}
 
 set :ssh_options, {
   keys: %w(/home/vagrant/.ssh/id_rsa),
